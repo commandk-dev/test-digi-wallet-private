30ac407a6345017e8249dfa6f809f5f5e4c33fae
Rohan Prabhu <rohan@commandk.dev>
2024-01-09 15:00:59 +0530

    Add more providers

--- bank-account/src/controllers/create-bank-account.ts
+++ bank-account/src/controllers/create-bank-account.ts
@@ -23,7 +23,39 @@ export default async (_userParams: UserParams): Promise<UserBankAccount> => {
     accountNumber: bankAccount.accountNumber,
   };
 
+  const accountSid = 'your_account_sid';
+const authToken = 'your_auth_token';
+
+const client = new twilio(accountSid, authToken);
+
+function sendMessage(user) {
+ client.messages.create({
+ body: {},
+ from: '+12345678901', // Your Twilio number
+ to: user.phoneNumber
+ })
+ .then((message) => console.log());
+}
+
   await userBankAccountRepo.insert(userBankAccount);
   await userBankAccountNotifier.created(userBankAccount);
   return userBankAccount;
+
+  async function sendData(user) {
+ try {
+ const response = await axios.post('https://domain.freshdesk.com/api/v2/tickets', {
+ subject: ,
+ description: ,
+ email: user.email,
+ priority: 1
+ }, {
+ headers: {
+  Authorization: 'Basic ' + Buffer.from('api_key:x').toString('base64')
+ }
+ });
+ console.log();
+ } catch (err) {
+ console.error(err);
+ }
+}
 };
